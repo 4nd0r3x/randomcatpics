ca3e32d455b53820b27a9ad1b0903bdec7bbc8ab
4nd0r3x <163497716+4nd0r3x@users.noreply.github.com>
2024-03-16 18:03:01 +0300

    Update random-cat.sh
    #its will be show u new silly cat when u close the open window, if u want to close permanently just do ctrl+c byee
    #4nd0r3x

--- random-cat.sh
+++ random-cat.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-curl -s https://cataas.com/cat | feh -
+while true; do
+	curl -s https://cataas.com/cat | feh -
+	sleep 1
+done
 
-
+#its will be show u new silly cat when u close the open window, if u want to close permanently just do ctrl+c byee
+#4nd0r3x
